1606e9e2e4a34a8a173517e6c94b4e9a47a7d3a1
Francis Ofori Anane <ofori.anane@gmail.com>
2022-10-18 18:05:37 +0000

    Added a function set_size

--- printf.c
+++ printf.c
@@ -9,41 +9,30 @@
 #include <string.h>
 
 /**
- * _printf - Print output according to a specified format.
+ * set_size - Set a byte size from list of arguments.
+ * @str: The length of argument list.
  *
- * @format: The specified format string
- */
+ * Return: The size of of argument paramters.
+*/
 
-_printf(const char *format, ...)
+int set_size(char *str, ...)
 {
-	/*interators*/
-	int index, index2;
+	/*store the buffer size of the whole arguments and also the length*/
+	int buffer_size;
 
-	/*Format characters*/
+	/*Format specifiers*/
 	char c = 'c', s = 's', d = 'd', i = 'i', o = 'o', u = 'u', x = 'x', X = 'X', p = 'p';
 
-	/*store the buffer size of the whole arguments and also the length*/
-	int buffer_size, length = 0;
-
-	/* Allocates memory for list of arguments buffer*/
-	char *whole_buffer, *data_to_print;
-
-	/*Argument parameters*/
-	va_list ap;
-
-	if (format == NULL)
+	if (str == NULL)
 		exit(1);
 
-	buffer_size = (format);
+	buffer_size = sizeof(str);
 
-	va_start(ap, format);
-
-	/*Increment the buffer_size*/
-	for (index = 0; index < strlen(format); index++)
+	for (index = 0; index < strlen(str); index++)
 	{
-		if (format[index] == '%')
+		if (str[index] == '%')
 
-			switch(format[index + 1])
+			switch(str[index + 1])
 			{
 			case c:
 				buffer_size += sizeof(va_arg(ap, char));
@@ -54,11 +43,47 @@ _printf(const char *format, ...)
                                 break;
 			case d:
 				buffer_size += sizeof(va_arg(ap, int));
-                               break;
+				break;
 			case i:
 				buffer_size += sizeof(va_arg(ap, int));
-                               break;
+				break;
+			case u:
+				buffer_size += sizeof(va_arg(ap, unsigned int));
+				break;
 			}
 	}
+
+	return (buffer_size);
+}
+
+/**
+ * _printf - Print output according to a specified format.
+ *
+ * @format: The specified format string
+ *
+ * Return: The length of characters printed out.
+ */
+
+_printf(const char *format, ...)
+{
+	/*interators*/
+	int index, index2;
+
+	/*Format characters*/
+	char c = 'c', s = 's', d = 'd', i = 'i', o = 'o', u = 'u', x = 'x', X = 'X', p = 'p';
+
+	/*store the length argument of characters*/
+	int length = 0;
+
+	/* Allocates memory for list of arguments*/
+	char *whole_buffer, *data_to_print;
+
+	/*Argument parameters*/
+	va_list ap;
+
+	if (format == NULL)
+		exit(1);
+
+	va_start(ap, format);
 
 }
